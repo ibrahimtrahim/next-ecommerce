3fd1da95b6fe1c553d3219bc812cead1eefb9e3a
ibrahimtrahim <trahimbrahim@gmail.com>
2024-09-11 22:01:18 +0000

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex justify-between items-center'>
+        <p>Logo</p>
+        <p>menu</p>
+    </nav>
+  )
+}
+
+export default Navbar
